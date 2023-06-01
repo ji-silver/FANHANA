2cc0737f5ae8d5bd96c6da8c25a2b8364a79e229
kyounghye <kyoung00828@gmail.com>
2023-06-02 00:21:52 +0900

    Feat:date picker 컴포넌트 작성 화면에 표시되는 format 함수에서 타입스크립트 오류가 발생하였습니다. 컴포넌트에 inline 속성을 적용하고, 클릭시 나오는 modal z-index 확인이 필요.

--- src/components/common/DatePicker/index.tsx
+++ src/components/common/DatePicker/index.tsx
@@ -1,1 +1,124 @@
+import React, { Dispatch, SetStateAction, useState } from "react";
+import DatePicker from "react-datepicker";
+import { format, getMonth, getYear } from "date-fns";
+import "react-datepicker/dist/react-datepicker.css";
 
+import styles from "./index.module.css";
+import "./index.css";
+
+interface Props {
+  selectedDate: Date | null;
+  setSelectedDate: Dispatch<SetStateAction<Date | null>>;
+}
+
+const YEARS = Array.from(
+  { length: getYear(new Date()) + 1 - 2000 },
+  (_, i) => getYear(new Date()) - i
+);
+
+const MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const DatePickerBox = () => {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleChange = (e: React.SetStateAction<Date | null>) => {
+    setIsOpen(!isOpen);
+    setSelectedDate(e);
+  };
+  const handleClick = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    setIsOpen(!isOpen);
+  };
+  const getDayOfWeek = (date: any) => {
+    //ex) getDayOfWeek('2022-06-13')
+    const week = ["일", "월", "화", "수", "목", "금", "토"];
+    const dayOfWeek = week[new Date(date).getDay()];
+    return dayOfWeek;
+  };
+
+  return (
+    <div className={styles.datePickerContainer}>
+      <button className={styles.datePicker} onClick={handleClick}>
+        {format(selectedDate, `yyyy.MM.dd (${getDayOfWeek(selectedDate)})`)}
+      </button>
+      {isOpen && (
+        <DatePicker
+          selected={selectedDate}
+          onChange={handleChange}
+          formatWeekDay={(nameOfDay) => nameOfDay.substring(0, 2)}
+          showYearDropdown
+          scrollableYearDropdown
+          shouldCloseOnSelect
+          yearDropdownItemNumber={100}
+          minDate={new Date("2000-01-01")}
+          maxDate={new Date()}
+          calendarClassName={styles.calenderWrapper}
+          dayClassName={(d) =>
+            d.getDate() === selectedDate!.getDate()
+              ? styles.selectedDay
+              : styles.unselectedDay
+          }
+          className={styles.datePicker}
+          renderCustomHeader={({
+            date,
+            changeYear,
+            decreaseMonth,
+            prevMonthButtonDisabled,
+            increaseMonth,
+            nextMonthButtonDisabled,
+          }) => (
+            <div className={styles.customHeaderContainer}>
+              <div className={styles.monthBox}>
+                <button
+                  type="button"
+                  onClick={decreaseMonth}
+                  className={styles.monthButton}
+                  disabled={prevMonthButtonDisabled}
+                >
+                  {"<"}
+                </button>
+                <span className={styles.month}>{MONTHS[getMonth(date)]}</span>
+                <button
+                  type="button"
+                  onClick={increaseMonth}
+                  className={styles.monthButton}
+                  disabled={nextMonthButtonDisabled}
+                >
+                  {">"}
+                </button>
+              </div>
+              <select
+                value={getYear(date)}
+                className={styles.year}
+                onChange={({ target: { value } }) => changeYear(+value)}
+              >
+                {YEARS.map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            </div>
+          )}
+          inline
+        />
+      )}
+    </div>
+  );
+};
+
+export default DatePickerBox;
