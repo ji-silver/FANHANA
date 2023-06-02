f71676d60f99d554d353ded344cbdef66b93e3e2
shine-son <schk9611@gmail.com>
2023-06-02 23:09:24 +0900

    Fix: 코드 스타일 통일, styled 코드는 아래로

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,6 +1,24 @@
 import React from "react";
 import styled from "styled-components";
 
+const Footer: React.FC = () => {
+  return (
+    <StyledFooter>
+      <FooterContent>
+        <p>
+          본 콘텐츠의 저작권은 엘리스에 있으며, 이를 무단 이용하는 경우 저작권법
+          등에 따라 법적 책임을 질 수 있습니다.
+        </p>
+        <CopyrightText>
+          &copy; {new Date().getFullYear()}. FanHana. All Rights Reserved.
+        </CopyrightText>
+      </FooterContent>
+    </StyledFooter>
+  );
+};
+
+export default Footer;
+
 const StyledFooter = styled.footer`
   background-color: #f2f2f2;
   padding: 20px;
@@ -24,21 +42,3 @@ const CopyrightText = styled.p`
   font-size: 14px;
   color: #666666;
 `;
-
-const Footer: React.FC = () => {
-  return (
-    <StyledFooter>
-      <FooterContent>
-        <p>
-          본 콘텐츠의 저작권은 엘리스에 있으며, 이를 무단 이용하는 경우 저작권법
-          등에 따라 법적 책임을 질 수 있습니다.
-        </p>
-        <CopyrightText>
-          &copy; {new Date().getFullYear()}. FanHana. All Rights Reserved.
-        </CopyrightText>
-      </FooterContent>
-    </StyledFooter>
-  );
-};
-
-export default Footer;
