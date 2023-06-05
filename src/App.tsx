5fb410f839e2b174bff3fc844a550e31d95069f8
shine-son <schk9611@gmail.com>
2023-06-05 17:00:24 +0900

    Fix: App.tsx에 Header, Footer 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./styles/App.css";
-import DatePickerBox from "./components/common/DatePickerBox/DatePickerBox";
-
+import Header from "./components/common/Header/Header";
+import Footer from "./components/common/Footer";
 import LoginPage from "./pages/login";
 import JoinPage from "./pages/join";
 
@@ -11,17 +11,19 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Header></Header>
         <Routes>
-          <Route path="/"></Route>
-          <Route path="/login" element={<LoginPage/>}></Route>
-          <Route path="/join" element={<JoinPage/>}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/join" element={<JoinPage />}></Route>
           <Route path="/register"></Route>
           <Route path="/mypage"></Route>
           <Route path="/soccer"></Route>
           <Route path="/baseball"></Route>
           <Route path="/esport"></Route>
           <Route path="/shorts"></Route>
+          <Route path="/" element={<LoginPage />}></Route>
         </Routes>
+        <Footer></Footer>
       </Router>
     </div>
   );
