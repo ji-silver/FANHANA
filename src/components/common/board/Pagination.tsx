f048b2934665746c5cd18442656b66897ead3e08
Rocket0000 <gyj960526@gmail.com>
2023-06-07 21:54:48 +0900

    Fix: 페이지네이션 버그 수정

--- src/components/common/board/Pagination.tsx
+++ src/components/common/board/Pagination.tsx
@@ -1,77 +1,72 @@
-import React, { Dispatch, SetStateAction, useState } from "react";
+import React, {Dispatch, SetStateAction} from "react";
 import styled from "styled-components";
 
-import { ReactComponent as Next } from "../../../assets/icons/Next.svg";
-import { ReactComponent as DoubleNext } from "../../../assets/icons/DoubleNext.svg";
-import { ReactComponent as Previous } from "../../../assets/icons/Previous.svg"
-import { ReactComponent as DoublePrevious } from "../../../assets/icons/DoublePrevious.svg"
+import {ReactComponent as Next} from "../../../assets/icons/Next.svg";
+import {ReactComponent as DoubleNext} from "../../../assets/icons/DoubleNext.svg";
+import {ReactComponent as Previous} from "../../../assets/icons/Previous.svg"
+import {ReactComponent as DoublePrevious} from "../../../assets/icons/DoublePrevious.svg"
 
 interface PaginationProps {
-  total : number;
-  limit : number;
-  page : number;
-  setPage : Dispatch<SetStateAction<number>>;//setState 값을 넘겨야해서 지정해준 타입 
+    total: number;
+    limit: number;
+    page: number;
+    setPage: Dispatch<SetStateAction<number>>;//setState 값을 넘겨야해서 지정해준 타입
 }
 
-const Pagination:React.FC<PaginationProps> = ({ total, limit, page, setPage }) => {
-  const numPages = Math.ceil(total / limit);
-  const showList = new Array(numPages).fill(0);
-  const [currPage, setCurrPage] = useState(page);
-  let firstNum = currPage - (currPage % 10) + 1;
-  let lastNum = currPage - (currPage % 10) + 10;
+const Pagination: React.FC<PaginationProps> = ({total, limit, page, setPage}) => {
 
-  console.log({"currPage is":currPage, "firsNum is" : firstNum, "page is" : page, "lastPage": lastNum, numPages })
-  showList.map(( _, idx ) => console.log("idx:::",idx))
-  
-  return (
-    <Nav>
-      <PageButton onClick={ () => {setPage( page - 5 ); setCurrPage( page - 2);}} disabled={page === 1}>
-        <DoublePrevious className="arrowBtn double" />
-      </PageButton>      
-      <PageButton onClick={ () => {setPage( page - 1 ); setCurrPage( page - 3);}} disabled={page === 1}>
-        <Previous className="arrowBtn" />
-      </PageButton>
-      <PageButton
-        onClick={ () => setPage( page - 1 ) }
-        className={ page === firstNum ?  "page" : "" }
-      >
-        {firstNum}
-      </PageButton>
-      {
-        showList.map(( _, idx ) => {
-          if(idx <= 8){
-            return(
-              <PageButton 
-              key={ idx + 1 }
-              onClick={ () => setPage(firstNum + 1 + idx)}
-              className={ page === firstNum + 1 + idx ?  "page" : "" }
-            >
-              { firstNum + 1 + idx }
+    const numPages = Math.ceil(total / limit);
+
+    const rangeSize = 10;
+    const nearestTen = Math.ceil(page / rangeSize) * rangeSize;
+
+    const startPage = nearestTen - rangeSize + 1;
+    const endPage = numPages % 10 === 0 ? nearestTen : numPages;
+    
+    const showList = Array.from({ length: endPage - startPage + 1 }, (_, i) => startPage + i);
+    console.log('showList:::', showList);
+    
+
+    return (
+        <Nav>
+            <PageButton onClick={() => {
+                const toPage = page - 5 < 1 ? 1 : page - 5
+                setPage(toPage);
+            }} disabled={page === 1}>
+                <DoublePrevious className="arrowBtn double"/>
             </PageButton>
-            )
-          }
-          else if(idx >= numPages){
-            return(
-              <PageButton 
-              key={ idx + 1 }
-              onClick={ () => setPage(lastNum)}
-              className={ page === lastNum ?  "page" : "" }
-            >
-              { lastNum }
+            <PageButton onClick={() => {
+                setPage(page - 1);
+            }} disabled={page === 1}>
+                <Previous className="arrowBtn"/>
             </PageButton>
-            )
-          }
-        })
-        
-      }
-      <PageButton onClick={ () => { setPage( page + 1 ); setCurrPage( page ) }} disabled={page === numPages}>
-        <Next className="arrowBtn" />
-      </PageButton>
-      <PageButton onClick={ () => { setPage( page + 5 ); setCurrPage( page ) }} disabled={page === numPages}>
-        <DoubleNext className="arrowBtn double next" />
-      </PageButton>
-    </Nav>
-  );
+            {
+                showList.map((value, index) => {
+                  
+                    return (  
+                                <PageButton
+                                    key={index}
+                                    onClick={() => {setPage(value); console.log("value:", value,"----------------")}}
+                                    className={ page === value ?  "page" : "" }
+                                >
+                                    {value}   
+                                </PageButton>
+                            )
+                }).splice(0, 10)
+            }
+            <PageButton onClick={() => {
+                setPage(page + 1);
+            }} disabled={page === numPages}>
+                <Next className="arrowBtn"/>
+            </PageButton>
+            <PageButton onClick={() => {
+                const toPage = page + 5 > numPages ? numPages : page + 5
+                setPage(toPage);
+            }} disabled={page === numPages}>
+                <DoubleNext className="arrowBtn double next"/>
+            </PageButton>
+        </Nav>
+    );
 }
 
 export default Pagination;
@@ -97,36 +92,41 @@ const PageButton = styled.button`
   font-size: 14px;
   font-weight: 500;
 
-  &:hover{
+  &:hover {
     background: #EFEAFC;
     color: #5F30E2;
     cursor: pointer;
   }
-  &:hover svg.arrowBtn path{
-    fill:#5F30E2;
+
+  &:hover svg.arrowBtn path {
+    fill: #5F30E2;
   }
-  &[disabled]{
+
+  &[disabled] {
     background: transparent;
     cursor: revert;
   }
-  &[disabled] svg.arrowBtn path{
+
+  &[disabled] svg.arrowBtn path {
     fill: #C7C9D9;
   }
-  &.page{
+
+  &.page {
     background: #5F30E2;
     color: #FFF;
     cursor: revert;
   }
 
-  svg.arrowBtn path{
+  svg.arrowBtn path {
     transform: translate(-5px, -1px);
     fill: #28293D;
   }
-  svg.arrowBtn.double path{
+
+  svg.arrowBtn.double path {
     transform: translate(-3px, -1px);
   }
-  svg.arrowBtn.double.next path{
+
+  svg.arrowBtn.double.next path {
     transform: translate(-8px, -1px);
   }
 `;
-
