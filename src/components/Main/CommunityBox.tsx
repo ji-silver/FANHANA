e3b8a0882da99870897d0575accaf7eb02bebe91
kyoung <kyoung00828@gmail.com>
2023-06-08 14:55:38 +0900

    Feat: 더미데이터, 기본 테이블 구성

--- src/components/Main/CommunityBox.tsx
+++ src/components/Main/CommunityBox.tsx
@@ -2,11 +2,25 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 
 import styles from "./main.module.scss";
+import communityData from "./communityData.json";
 
 const CommunityBox = () => {
   return (
     <>
-      <Community></Community>
+      <Community>
+        <div className={styles.title}>오늘의 커뮤니티</div>
+        <div>
+          <div>전체</div>
+          <table>
+            <tr>
+              <th>게시글ID</th>
+              <th></th>
+              <th>제목</th>
+              <th>조회수</th>
+            </tr>
+          </table>
+        </div>
+      </Community>
     </>
   );
 };
