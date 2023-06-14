c258cb421dd0bcc9ef517e996b36dd72460e0253
dbwjd <dbwjd7265@gmail.com>
2023-06-14 23:11:03 +0900

    Fix: 연결수정

--- src/pages/mypage/myPostsPage.tsx
+++ src/pages/mypage/myPostsPage.tsx
@@ -2,11 +2,17 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import Dropdown from "./../../components/common/Dropdown";
+import TableList from "./../../components/common/TableList";
+
 
 const MyPostsPage = () => {
   const [posts, setPosts] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState<any>(null);
   const token = localStorage.getItem("accessToken");
+  const [total, setTotal] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+
   const BASE_URL = "http://localhost:5500";
 
   useEffect(() => {
@@ -55,24 +61,9 @@ const MyPostsPage = () => {
     return decodedToken?.user_id;
   };
 
-  const getCategoryLabel = (category: any) => {
-    if (category === 0) {
-      console.log("축구선택");
-      return "축구";
-    } else if (category === 1) {
-      console.log("야구 선택");
-      return "야구";
-    } else if (category === 2) {
-      console.log("롤 선택");
-      return "롤";
-    } else if (category === 4 || category === "4") {
-      console.log("아무것도 선택안함");
-      return "전체";
-    }
-  };
-
   const handleDropdownSelect = (selectedItem: any) => {
     setSelectedCategory(selectedItem === 4 ? null : selectedItem);
+    setPage(1);
   };
 
   const filterPosts = () => {
@@ -82,6 +73,7 @@ const MyPostsPage = () => {
       return posts.filter((post: any) => post.category === parseInt(selectedCategory));
     }
   };
+ 
 
   return (
     <div className="mh500">
@@ -94,32 +86,14 @@ const MyPostsPage = () => {
         />
       </div>
       {posts.length > 0 ? (
-        <table style={{ width: "100%", color: "#333" }}>
-          <thead>
-            <tr style={{ borderBottom: "1px solid #E3E3E3" }}>
-              <th style={{width:"80px"}}>번호</th>
-              <th style={{width:"80px"}}>분류</th>
-              <th>제목</th>
-              <th style={{width:"120px"}}>작성일</th>
-              <th style={{width:"80px"}}>조회수</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filterPosts().map((post: any) => (
-              <tr key={post.id}>
-                <td>{post.id}</td>
-                <td>{getCategoryLabel(post.category)}</td>
-                <td style={{textAlign:"left"}}>
-                  <Link to={`http://localhost:5500/api/v1/post/${post.id}`}>
-                    {post.title}
-                  </Link>
-                </td>
-                <td>{post.created_at}</td>
-                <td>{post.views}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <TableList
+        show="my"
+        data={filterPosts()}
+        total={total}
+        limit={limit}
+        page={page}
+        setPage={setPage}
+      />
       ) : (
         <p>게시글이 없습니다.</p>
       )}
